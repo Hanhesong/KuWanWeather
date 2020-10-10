d46ff093046c4a50314c12b45c41cd3131dbaa51
hkevin <hkevin@hikeen.com.cn>
2020-10-10 15:29:59 +0800

    add Data class

---
+++ app/src/main/java/com/hhs/kuwanweather/db/County.java
@@ -0,0 +1,60 @@
+package com.hhs.kuwanweather.db;
+
+import org.litepal.exceptions.DataSupportException;
+
+import java.nio.channels.Pipe;
+
+public class County extends DataSupportException {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+
+    public County(String errorMessage) {
+        super(errorMessage);
+    }
+
+    @Override
+    public String toString() {
+        return "County{" +
+                "id=" + id +
+                ", countyName='" + countyName + '\'' +
+                ", weatherId='" + weatherId + '\'' +
+                ", cityId=" + cityId +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/hhs/kuwanweather/db/Province.java
@@ -0,0 +1,48 @@
+package com.hhs.kuwanweather.db;
+
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends DataSupportException {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;//省代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+    public Province(String errorMessage) {
+        super(errorMessage);
+    }
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceName='" + provinceName + '\'' +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+}
